cd94328f22f324f849c287e66c560512d6f07965
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2024-09-09 12:43:19 +0200

    Improve comments and formatting of functions.php

--- functions.php
+++ functions.php
@@ -1,27 +1,30 @@
 <?php
 
 /**
- * Load the active theme's style.css file.
+ * Load the active theme's style.css file on the front end of the website.
  *
- * Source:
- * - https://developer.wordpress.org/themes/core-concepts/including-assets/#front-end-stylesheets
- * - https://developer.wordpress.org/themes/advanced-topics/child-themes/#loading-style-css
+ * This is necessary if
+ * - the parent theme only loads its own stylesheet, or
+ * - the parent theme doesn't load a stylesheet at all (e.g. Twenty Twenty-Four theme)
+ *
+ * @link https://developer.wordpress.org/themes/core-concepts/including-assets/#front-end-stylesheets
+ * @link https://developer.wordpress.org/themes/advanced-topics/child-themes/#loading-style-css
  */
 
-add_action("wp_enqueue_scripts", "mytheme_enqueue_styles");
-
-function mytheme_enqueue_styles() {
-	wp_enqueue_style("mytheme-style", get_stylesheet_uri());
+function themeslug_enqueue_styles() {
+	wp_enqueue_style( 'themeslug-style', get_stylesheet_uri() );
 }
+
+add_action( 'wp_enqueue_scripts', 'themeslug_enqueue_styles' );
 
 /**
- * Make styles from the active theme's style.css to appear in the editor.
+ * Load the active theme's style.css file in the editor.
  *
- * Source: https://developer.wordpress.org/themes/core-concepts/including-assets/#editor-stylesheets
+ * @link https://developer.wordpress.org/themes/core-concepts/including-assets/#editor-stylesheets
  */
 
-add_action("after_setup_theme", "mytheme_setup");
-
-function mytheme_setup() {
-	add_editor_style(get_stylesheet_uri());
+function themeslug_setup() {
+	add_editor_style( get_stylesheet_uri() );
 }
+
+add_action( 'after_setup_theme', 'themeslug_setup' );
