a134804dbfeae9364cc3b01420dea386bb6b72e1
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2024-09-17 12:22:51 +0200

    Update stylelint.config.js

--- stylelint.config.js
+++ stylelint.config.js
@@ -14,7 +14,12 @@ export default {
 		"media-feature-name-value-no-unknown": true,
 		"no-unknown-animations": true,
 		"no-unknown-custom-media": true,
-		"no-unknown-custom-properties": true,
+		"no-descending-specificity": [
+			true,
+			{
+				severity: "warning",
+			},
+		],
 
 		// Enforce conventions: Allowed, disallowed & required
 		"color-named": [
@@ -24,74 +29,14 @@ export default {
 			},
 		],
 
-		// Enforce conventions: Max & Min
-		"max-nesting-depth": [
-			3,
-			{
-				ignore: ["blockless-at-rules", "pseudo-classes"],
-				severity: "warning",
-			},
-		],
-		"selector-max-attribute": [
-			3,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-class": [
-			3,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-combinators": [
-			2,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-compound-selectors": [
-			4,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-id": [
-			1,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-pseudo-class": [
-			2,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-specificity": [
-			"1,3,2",
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-type": [
-			2,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-universal": [
-			2,
-			{
-				severity: "warning",
-			},
-		],
+		// Notation
+		"media-feature-range-notation": null,
 
 		// Enforce conventions: Patterns
-		// "custom-media-pattern": null,
-		// "custom-property-pattern": null,
-		// "keyframes-name-pattern": null,
-		// "selector-class-pattern": null,
-		// "selector-id-pattern": null,
+		"custom-media-pattern": null,
+		"custom-property-pattern": null,
+		"keyframes-name-pattern": null,
+		"selector-class-pattern": null,
+		"selector-id-pattern": null,
 	},
 };
